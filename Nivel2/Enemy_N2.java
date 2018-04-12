efbd7b440144dd3158ade7dc5b0a9140ad137dd8
Luis Felipe Miranda <luisfmiranda97@gmail.com>
2018-04-11 23:39:32 -0500

    NIVEL DOS JAVADOC
    JAVADOC

--- Nivel2/Enemy_N2.java
+++ Nivel2/Enemy_N2.java
@@ -1,8 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+ENEMY IN LEVEL TWO ADMINISTRATION CLASS GIVEN THE SPECIFIC GAME LEVEL MECHANICS
+*/
 package Nivel2;
 
 import Control.Nivel;
