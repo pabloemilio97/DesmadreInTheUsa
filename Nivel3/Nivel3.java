2737ee5945b0a139d1c76f9d310a46c49f0415f1
Luis Felipe Miranda <luisfmiranda97@gmail.com>
2018-04-11 23:44:29 -0500

    Seg Commit
    ahi te va perro

--- Nivel3/Nivel3.java
+++ Nivel3/Nivel3.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+LEVEL THREE ADMINISTRATION CLASS FOR THE LEVEL PURPOSES
  */
 package Nivel3;
 
