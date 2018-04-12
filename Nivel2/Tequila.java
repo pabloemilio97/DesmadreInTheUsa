28c1292d3bda56474f07feea3f8a1d45b3da2b83
Radtke <kevinradtkeg@gmail.com>
2018-04-11 20:53:51 -0500

    Clases de Nivel 2 con constructores y metodos basicos

---
+++ Nivel2/Tequila.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Nivel2;
+
+/**
+ *
+ * @author macuser
+ */
+public class Tequila {
+    private int posX;
+    private int posY;
+    
+    public Tequila(int x, int y){
+        this.posX = x;
+        this.posY = y;
+    }
+}
