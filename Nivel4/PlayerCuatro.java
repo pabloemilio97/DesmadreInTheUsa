f22282299ae7457579f1cc49dbfc57ff52a8ba47
Luis Felipe Miranda <luisfmiranda97@gmail.com>
2018-04-11 23:34:06 -0500

    JAVADOC Y DESCRIPCION DE CLASES
    Falta la descripcion para nivel 2

--- Nivel4/PlayerCuatro.java
+++ Nivel4/PlayerCuatro.java
@@ -1,8 +1,7 @@
+
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+PLAYERS IN LEVEL FOUR SPECIFICATIONS AND ADMINISTRATION CLASS FOR THE PURPOSE OF GIVEN LEVEL GAME MECHANICS
+*/
 package Nivel4;
 
 import Control.Nivel;
