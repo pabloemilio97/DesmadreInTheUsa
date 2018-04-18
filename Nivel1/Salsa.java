20d9996022e0304a071979b80455b649ce96582f
Pablo Andrade <pabloemilioandrade@gmail.com>
2018-04-18 11:06:47 -0500

    creado de clase salsa

---
+++ Nivel1/Salsa.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Nivel1;
+
+import Control.Nivel;
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author pablo
+ */
+public class Salsa extends Control.Item{
+    public Salsa(int x, int y, int width, int height, BufferedImage defaultImage, Nivel game) {
+        super(x, y, width, height, defaultImage, game);
+    }
+
+    @Override
+    public void tick() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
