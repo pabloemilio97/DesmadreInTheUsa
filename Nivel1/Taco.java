2dee43b90bd40ed00123ae902cf00bfc1acff2a8
Pablo Andrade <pabloemilioandrade@gmail.com>
2018-04-18 12:19:49 -0500

    movimiento circular en taco, v1

--- Nivel1/Taco.java
+++ Nivel1/Taco.java
@@ -15,21 +15,23 @@ import java.awt.image.BufferedImage;
  */
 public class Taco extends Control.Item{
     private int velocidad; //velocidad del taco
+    private int degrees;
     
     public Taco(int x, int y, int width, int height, BufferedImage defaultImage, Nivel game) {
         super(x, y, width, height, defaultImage, game);
+        degrees = 0;
     }
     
     protected Point getPointOnCircle(float degress, float radius) {
 
-        int x = Math.round(getWidth() / 2);
-        int y = Math.round(getHeight() / 2);
+        int X = Math.round(getWidth() / 2);
+        int Y = Math.round(getHeight() / 2);
 
         double rads = Math.toRadians(degress - 90); // 0 becomes the top
 
         // Calculate the outter point of the line
-        int xPosy = Math.round((float) (x + Math.cos(rads) * radius));
-        int yPosy = Math.round((float) (y + Math.sin(rads) * radius));
+        int xPosy = Math.round((float) (X + Math.cos(rads) * radius));
+        int yPosy = Math.round((float) (Y + Math.sin(rads) * radius));
 
         return new Point(xPosy, yPosy);
 
@@ -37,7 +39,13 @@ public class Taco extends Control.Item{
     
     @Override
     public void tick() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        int diameter = Math.min(getWidth(), getHeight());
+        this.x = (getWidth() - diameter) / 2;
+        this.y = (getHeight() - diameter) / 2;
+        float innerDiameter = 20;
+
+        Point p = getPointOnCircle(degrees, (diameter / 2f) - (innerDiameter / 2));
     }
     
 }
