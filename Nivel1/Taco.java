3f971efda2a72a7a196e61f77ddb7023752e934d
Pablo Andrade <pabloemilioandrade@gmail.com>
2018-04-18 12:58:46 -0500

    cambio en taco, movimiento circular

--- Nivel1/Taco.java
+++ Nivel1/Taco.java
@@ -23,12 +23,24 @@ public class Taco extends Control.Item{
     
     @Override
     public void tick() {
-        int rad = 10;
-        float angle = 50;
+        int rad = 200;       //Radius in px
+        float angle = 0;     //Angle (this needs to be represented in radians, not degrees)
+        float centreX;       //X Coordinate of the centre of rotation
+        float centreY;       //Y Coordinate of the centre of rotation
+        float time = 2;          //Number of seconds a full revolution should take
+        int ticksPerSecond = 50; //Number of game updates per second
 
-        this.x -= Math.round(rad * Math.cos(angle));
-        this.y += Math.round(rad * Math.sin(angle));
-        angle += .1;
+        float spriteX;       //X Position of sprite
+        float spriteY;       //Y Position of sprite
+        //Update the angle
+        angle+= 2 * Math.PI / (ticksPerSecond * time);
+
+        //Update the coordinates
+        spriteX = (centreX  + (rad*Math.cos(angle)));
+        spriteY = (centreY  + (rad*Math.sin(angle)));
+        
+        //If you want to set the initial angle of the object around the orbit-path, then simply specify in radians
+        angle = (angle in degrees)/180 * Math.PI
     }
     
 }
