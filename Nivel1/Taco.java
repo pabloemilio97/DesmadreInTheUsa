c078cc25c834944388b49c69b10df0a44e9789ca
Pablo Andrade <pabloemilioandrade@gmail.com>
2018-04-18 11:58:46 -0500

    agregado emtodo getPoint en taco

--- Nivel1/Taco.java
+++ Nivel1/Taco.java
@@ -6,6 +6,7 @@
 package Nivel1;
 
 import Control.Nivel;
+import java.awt.Point;
 import java.awt.image.BufferedImage;
 
 /**
@@ -13,11 +14,27 @@ import java.awt.image.BufferedImage;
  * @author adanlopezalatorre
  */
 public class Taco extends Control.Item{
-
+    private int velocidad; //velocidad del taco
+    
     public Taco(int x, int y, int width, int height, BufferedImage defaultImage, Nivel game) {
         super(x, y, width, height, defaultImage, game);
     }
+    
+    protected Point getPointOnCircle(float degress, float radius) {
 
+        int x = Math.round(getWidth() / 2);
+        int y = Math.round(getHeight() / 2);
+
+        double rads = Math.toRadians(degress - 90); // 0 becomes the top
+
+        // Calculate the outter point of the line
+        int xPosy = Math.round((float) (x + Math.cos(rads) * radius));
+        int yPosy = Math.round((float) (y + Math.sin(rads) * radius));
+
+        return new Point(xPosy, yPosy);
+
+    }
+    
     @Override
     public void tick() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
