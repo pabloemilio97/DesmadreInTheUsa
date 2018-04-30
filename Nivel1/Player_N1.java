9e66e5fd94e5530a1dcb21859ff129ced5205985
Pablo Andrade <pabloemilioandrade@gmail.com>
2018-04-30 15:29:35 -0500

    added comments to playern1

--- Nivel1/Player_N1.java
+++ Nivel1/Player_N1.java
@@ -15,14 +15,30 @@ import java.awt.image.BufferedImage;
  * @author adanlopezalatorre
  */
 public class Player_N1 extends Control.Player{
-    
+    /**
+     * constructor for playerN1
+     * @param x
+     * @param y
+     * @param width
+     * @param height
+     * @param spritePath
+     * @param frames
+     * @param game
+     * @param actionSound 
+     */
     public Player_N1(int x, int y, int width, int height, String spritePath, int frames, Nivel game, SoundClip actionSound) {
         super(x, y, width, height, spritePath, frames, game, actionSound);
     }
+    /**
+     * copy constructor
+     * @param player 
+     */
     public Player_N1(Control.Player player){
         super(player);
     }
-    
+    /**
+     * tick for player
+     */
     @Override
     public void tick() {
         
