1a4636824a5b55953e408ce0c08433c25629b2fe
Adán López Alatorre <adanlopezalatorre@Adans-MacBook-Pro-2.local>
2018-05-02 08:23:25 -0500

    dd

--- Control/Nivel.java
+++ Control/Nivel.java
@@ -15,7 +15,7 @@ import javax.swing.JLabel;
 
 public abstract class Nivel implements Runnable{
     
-    public static int width = 700, height = 700, nivelTime = 10;
+    public static int width = 700, height = 700, nivelTime = 100;
     
     protected int lifestock;
     protected Display display;
